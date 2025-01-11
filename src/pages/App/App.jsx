6693681be4313748fad05f889e342f28cfef33fa
johntrinhvu <johntrinhvu@yahoo.com>
2025-01-10 20:17:43 -0800

    get tailwind to function properly

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -2,10 +2,8 @@ import './App.css';
 
 export default function App() {
   return (
-    <div className="App">
-      <div>
-        <h1>Hello World</h1>
-      </div>
+    <div className="flex justify-center">
+      <h1>Hello World</h1>
     </div>
   );
 }
